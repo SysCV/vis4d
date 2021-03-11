c49962d00981c25bff17cb2b3eb63944347daaaa
Fisher Yu <fyu@users.noreply.github.com>
2021-03-11 11:41:14 +0100

    Split the github action to lint and test (#5)
    * Split the github action to lint and test
    
    * add lint dependency
    
    * add test name
    
    * update conda instructions / script
    
    Co-authored-by: tobiasfshr <tobias.fischer@vision.ee.ethz.ch>

--- scripts/install_linux_cpu.sh
+++ scripts/install_linux_cpu.sh
@@ -1,5 +1,4 @@
 python3 -m pip install --ignore-installed -r scripts/requirements.txt
 python3 -m pip install torch==1.7.1+cpu torchvision==0.8.2+cpu \
     torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
-python3 -m pip install detectron2 -f \
-    https://dl.fbaipublicfiles.com/detectron2/wheels/cpu/torch1.7/index.html
+python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
