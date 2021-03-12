89cf34c524530a23c86966955459cc57e4fa7827
Fisher Yu <i@yf.io>
2021-03-12 15:21:08 +0100

    update conda setup

--- scripts/setup_linux_cpu_conda.sh
+++ scripts/setup_linux_cpu_conda.sh
@@ -2,7 +2,8 @@
 eval "$(conda shell.bash hook)"
 conda create --name openmt python=3.8
 conda activate openmt
-python -m pip install --ignore-installed  -r requirements.txt
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cpuonly -c pytorch
-python -m pip install detectron2 -f \
-  https://dl.fbaipublicfiles.com/detectron2/wheels/cpu/torch1.7/index.html
+python3 -m pip install --ignore-installed  -r requirements.txt
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
+# python -m pip install detectron2 -f \
+#   https://dl.fbaipublicfiles.com/detectron2/wheels/cpu/torch1.7/index.html
+python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
