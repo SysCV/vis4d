941fb980562102823af37423ef6dd8a4db74d5e5
Fisher Yu <i@yf.io>
2021-03-12 15:06:43 +0100

    update pytorch

--- scripts/setup_linux_cpu.sh
+++ scripts/setup_linux_cpu.sh
@@ -1,5 +1,5 @@
 python3 -m pip install --ignore-installed -r requirements.txt
-python3 -m pip install torch==1.7.1+cpu torchvision==0.8.2+cpu \
-    torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.8.0+cpu torchvision==0.9.0+cpu torchaudio==0.8.0 \
+  -f https://download.pytorch.org/whl/torch_stable.html
 python -m pip install detectron2 -f \
   https://dl.fbaipublicfiles.com/detectron2/wheels/cpu/torch1.7/index.html
