eef214406d3f6a56cd7ff350e222378f091180d9
Fisher Yu <i@yf.io>
2021-07-23 12:26:07 +0200

    lint tools

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,5 +1,5 @@
 python3 -m black vist tools
 python3 -m isort vist tools
-python3 -m pylint vist tools
+python3 -m pylint vist tools/*
 python3 -m pydocstyle --convention=google vist tools
 python3 -m mypy --strict vist tools
