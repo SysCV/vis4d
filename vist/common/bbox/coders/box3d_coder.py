fix rotation loss, draw box3d issues

vist/common/bbox/coders/box3d_coder.py
@@ -7,7 +7,12 @@ import torch
 from vist.struct import Boxes2D, Boxes3D, Intrinsics
 
 from ...geometry.projection import project_points, unproject_points
-from ...geometry.rotation import alpha2yaw, rotation_output_to_alpha, yaw2alpha
+from ...geometry.rotation import (
+    alpha2yaw,
+    normalize_angle,
+    rotation_output_to_alpha,
+    yaw2alpha,
+)
 from .base import BaseBoxCoder3D, BaseBoxCoderConfig
 
 
@@ -17,6 +22,8 @@ class QD3DTBox3DCoderConfig(BaseBoxCoderConfig):
     center_scale: float = 10.0
     depth_log_scale: float = 2.0
     dim_log_scale: float = 2.0
+    num_rotation_bins: int = 2
+    bin_overlap: float = 1 / 6
 
 
 class QD3DTBox3DCoder(BaseBoxCoder3D):
@@ -53,18 +60,34 @@ class QD3DTBox3DCoder(BaseBoxCoder3D):
             dims = torch.log(targets_.dimensions) * self.cfg.dim_log_scale
 
             # rotation
+            num_bins, bin_overlap = (
+                self.cfg.num_rotation_bins,
+                self.cfg.bin_overlap,
+            )
             alpha = yaw2alpha(targets_.rot_y, targets_.center)
-
-            bin_cls = torch.zeros((alpha.shape[0], 2), device=alpha.device)
-            bin_res = torch.zeros((alpha.shape[0], 2), device=alpha.device)
+            bin_cls = torch.zeros(
+                (alpha.shape[0], num_bins), device=alpha.device
+            )
+            bin_res = torch.zeros(
+                (alpha.shape[0], num_bins), device=alpha.device
+            )
+            bin_centers = torch.arange(
+                -np.pi, np.pi, 2 * np.pi / num_bins, device=alpha.device
+            )
+            bin_centers += np.pi / num_bins
             for i in range(alpha.shape[0]):
-                if alpha[i] < np.pi / 6.0 or alpha[i] > 5 * np.pi / 6.0:
-                    bin_cls[i, 0] = 1
-                    bin_res[i, 0] = alpha[i] - (-0.5 * np.pi)
-
-                if alpha[i] > -np.pi / 6.0 or alpha[i] < -5 * np.pi / 6.0:
-                    bin_cls[i, 1] = 1
-                    bin_res[i, 1] = alpha[i] - (0.5 * np.pi)
+                overlap_value = np.pi * 2 / num_bins * bin_overlap
+                alpha_hi = normalize_angle(alpha[i] + overlap_value)
+                alpha_lo = normalize_angle(alpha[i] - overlap_value)
+                for bin_idx in range(self.cfg.num_rotation_bins):
+                    bin_min = bin_centers[bin_idx] - np.pi / num_bins
+                    bin_max = bin_centers[bin_idx] + np.pi / num_bins
+                    if (
+                        bin_min <= alpha_lo <= bin_max
+                        or bin_min <= alpha_hi <= bin_max
+                    ):
+                        bin_cls[i, bin_idx] = 1
+                        bin_res[i, bin_idx] = alpha[i] - bin_centers[bin_idx]
 
             result.append(
                 torch.cat([delta_center, depth, dims, bin_cls, bin_res], -1)

vist/common/geometry/rotation.py
@@ -3,6 +3,11 @@ import numpy as np
 import torch
 
 
+def normalize_angle(input_angles: torch.Tensor) -> torch.Tensor:
+    """Normalize content of input_angles to range [-pi, pi]."""
+    return (input_angles + np.pi) % (2 * np.pi) - np.pi
+
+
 def yaw2alpha(rot_y: torch.Tensor, center: torch.Tensor) -> torch.Tensor:
     """Get alpha by rotation_y - theta.
 
@@ -13,10 +18,8 @@ def yaw2alpha(rot_y: torch.Tensor, center: torch.Tensor) -> torch.Tensor:
     Returns:
         alpha: Observation angle of object, ranging [-pi..pi]
     """
-    torch_pi = rot_y.new_tensor([np.pi])
     alpha = rot_y - torch.atan2(center[..., 0], center[..., 2])
-    alpha = (alpha + torch_pi) % (2 * torch_pi) - torch_pi
-    return alpha
+    return normalize_angle(alpha)
 
 
 def alpha2yaw(alpha: torch.Tensor, center: torch.Tensor) -> torch.Tensor:
@@ -29,51 +32,59 @@ def alpha2yaw(alpha: torch.Tensor, center: torch.Tensor) -> torch.Tensor:
     Returns:
         rot_y: Rotation around Y-axis in camera coordinates [-pi..pi]
     """
-    torch_pi = alpha.new_tensor([np.pi])
     rot_y = alpha + torch.atan2(center[..., 0], center[..., 2])
-    rot_y = (rot_y + torch_pi) % (2 * torch_pi) - torch_pi
-    return rot_y
+    return normalize_angle(rot_y)
 
 
-def rotation_output_to_alpha(output: torch.Tensor) -> torch.Tensor:
+def rotation_output_to_alpha(
+    output: torch.Tensor, num_bins: int = 2
+) -> torch.Tensor:
     """Get alpha from bin-based regression output.
 
     Uses method described in (with two bins):
     See: 3D Bounding Box Estimation Using Deep Learning and Geometry,
      Mousavian et al., CVPR'17
     """
-    idx = (output[:, 0] > output[:, 1]).float()
-    alpha1 = torch.atan(output[:, 2] / output[:, 3]) + (-0.5 * np.pi)
-    alpha2 = torch.atan(output[:, 4] / output[:, 5]) + (0.5 * np.pi)
-    return alpha1 * idx + alpha2 * (1 - idx)
+    out_range = torch.tensor(list(range(len(output))), device=output.device)
+    bin_idx = output[:, :num_bins].argmax(dim=-1)
+    res_idx = num_bins * (bin_idx + 1)
+    bin_centers = torch.arange(
+        -np.pi, np.pi, 2 * np.pi / num_bins, device=output.device
+    )
+    bin_centers += np.pi / num_bins
+    alpha = (
+        torch.atan(output[out_range, res_idx] / output[out_range, res_idx + 1])
+        + bin_centers[bin_idx]
+    )
+    return alpha
 
 
-def generate_rotation_output(pred: torch.Tensor) -> torch.Tensor:
+def generate_rotation_output(
+    pred: torch.Tensor, num_bins: int = 2
+) -> torch.Tensor:
     """Convert output to bin confidence and cos / sin of residual.
 
-    The viewpoint (alpha) prediction (N, 6) consists of:
-    bin confidences (N, 2): softmax logits for bin probability.
+    The viewpoint (alpha) prediction (N, num_bins + 2 * num_bins) consists of:
+    bin confidences (N, num_bins): softmax logits for bin probability.
         1st entry is probability for orientation being in bin 1,
         2nd entry is probability for orientation being in bin 2.
-    bin1 residual (N, 2): angle residual w.r.t. bin1 orientation,
+    bin residual (N, num_bins * 2): angle residual w.r.t. bin N orientation,
         represented as sin and cos values.
-    bin2 residual (N, 2): like above for bin2.
 
     See: 3D Bounding Box Estimation Using Deep Learning and Geometry,
      Mousavian et al., CVPR'17
     """
-    pred = pred.view(pred.size(0), -1, 6)
-    bin_logits = pred[..., 0:2]
+    pred = pred.view(pred.size(0), -1, num_bins + 2 * num_bins)
+    bin_logits = pred[..., :num_bins]
 
-    # bin 1 residuals
-    norm1 = pred[..., 2:4].norm(dim=-1, keepdim=True)
-    b1sin = pred[..., 2:3] / norm1
-    b1cos = pred[..., 3:4] / norm1
+    bin_residuals = []
+    for i in range(num_bins):
+        res_idx = num_bins * (i + 1)
+        norm = pred[..., res_idx : res_idx + 2].norm(dim=-1, keepdim=True)
+        bsin = pred[..., res_idx : res_idx + 1] / norm
+        bcos = pred[..., res_idx + 1 : res_idx + 2] / norm
+        bin_residuals.append(bsin)
+        bin_residuals.append(bcos)
 
-    # bin 2 residuals
-    norm2 = pred[..., 4:6].norm(dim=-1, keepdim=True)
-    b2sin = pred[..., 4:5] / norm2
-    b2cos = pred[..., 5:6] / norm2
-
-    rot = torch.cat([bin_logits, b1sin, b1cos, b2sin, b2cos], -1)
+    rot = torch.cat([bin_logits, *bin_residuals], -1)
     return rot

vist/vis/image.py
@@ -4,7 +4,6 @@ from typing import List, Optional, Tuple, Union
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
-from scalabel.label.utils import project_points_to_image
 
 from vist.struct import Intrinsics, NDArrayF64
 
@@ -110,31 +109,69 @@ def draw_bbox3d(
     intrinsics: NDArrayF64,
     color: Tuple[int],
     label: Optional[str] = None,
+    camera_near_clip: float = 0.15,
 ) -> None:  # pragma: no cover
     """Draw 3D box onto image."""
     draw = ImageDraw.Draw(image)
-    corners = project_points_to_image(box3d_to_corners(box3d), intrinsics)
+    corners = box3d_to_corners(box3d)
+    corners_proj = corners / corners[:, 2:3]
+    corners_proj = np.dot(corners_proj, intrinsics.T)  # type: ignore
+
+    def draw_line(
+        point1: NDArrayF64, point2: NDArrayF64, color: Tuple[int]
+    ) -> None:
+        if point1[2] < camera_near_clip and point2[2] < camera_near_clip:
+            return
+        if point1[2] < camera_near_clip:
+            point1 = get_intersection_point(point1, point2, camera_near_clip)
+        elif point2[2] < camera_near_clip:
+            point2 = get_intersection_point(point1, point2, camera_near_clip)
+        draw.line((tuple(point1[:2]), tuple(point2[:2])), fill=color)
 
     def draw_rect(selected_corners: NDArrayF64) -> None:
         prev = selected_corners[-1]
         for corner in selected_corners:
-            draw.line((tuple(prev), tuple(corner)), fill=color)
+            draw_line(prev, corner, color)
             prev = corner
 
     # Draw the sides
     for i in range(4):
-        draw.line((tuple(corners[i]), tuple(corners[i + 4])), fill=color)
+        draw_line(corners_proj[i], corners_proj[i + 4], color)
 
     # Draw bottom (first 4 corners) and top (last 4 corners)
-    draw_rect(corners[:4])
-    draw_rect(corners[4:])
+    draw_rect(corners_proj[:4])
+    draw_rect(corners_proj[4:])
 
     # Draw line indicating the front
-    center_bottom_forward = np.mean(corners[:2], axis=0)
-    center_bottom = np.mean(corners[:4], axis=0)
-    draw.line((tuple(center_bottom), tuple(center_bottom_forward)), fill=color)
+    center_bottom_forward = np.mean(corners_proj[:2], axis=0)
+    center_bottom = np.mean(corners_proj[:4], axis=0)
+    draw_line(center_bottom, center_bottom_forward, color)
 
     if label is not None:
         font = ImageFont.load_default()
-        center_top_forward = tuple(np.mean(corners[2:4], axis=0))
+        center_top_forward = tuple(np.mean(corners_proj[2:4], axis=0)[:2])
         draw.text(center_top_forward, label, (255, 255, 255), font=font)
+
+
+def get_intersection_point(
+    point1: NDArrayF64, point2: NDArrayF64, camera_near_clip: float
+) -> NDArrayF64:  # pragma: no cover
+    """Get point intersecting with camera near plane on line point1 -> point2.
+
+    The line is defined by two points (3 dimensional) in camera coordinates.
+    """
+    cam_dir = np.array([0, 0, 1])
+    center_pt: NDArrayF64 = cam_dir * camera_near_clip
+
+    c1, c2, c3 = center_pt
+    a1, a2, a3 = cam_dir
+    x1, y1, z1 = point1
+    x2, y2, z2 = point2
+
+    k_up = abs(a1 * (x1 - c1) + a2 * (y1 - c2) + a3 * (z1 - c3))
+    k_down = abs(a1 * (x1 - x2) + a2 * (y1 - y2) + a3 * (z1 - z2))
+    if k_up > k_down:
+        k = 1
+    else:
+        k = k_up / k_down
+    return (1 - k) * point1 + k * point2  # type: ignore