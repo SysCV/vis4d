ecb79ea78e8a5b5007893b6b4c25cc9cecc9b5c4
tobiasfshr <tobias.fischer@vision.ee.ethz.ch>
2021-11-02 15:26:05 +0100

    switch git:// with https://

--- scripts/install_cpu_conda_dep_full.sh
+++ scripts/install_cpu_conda_dep_full.sh
@@ -6,5 +6,5 @@ conda install pytorch torchvision torchaudio cpuonly -c pytorch
 python3 -m pip install --ignore-installed  -r requirements.txt
 python3 -m pip install mmcv-full
 python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
-python3 -m pip install 'git+git://github.com/SysCV/mmdetection.git'
-python3 -m pip install 'git+git://github.com/open-mmlab/mmsegmentation.git'
+python3 -m pip install 'git+https://github.com/SysCV/mmdetection.git'
+python3 -m pip install 'git+https://github.com/open-mmlab/mmsegmentation.git'
