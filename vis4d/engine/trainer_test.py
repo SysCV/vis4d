9ecc86701be109ade1bdf8ff62bc6b5981667b65
Thomas Huang <thomas.huang@vision.ee.ethz.ch>
2021-12-07 01:04:06 +0100

    Updating mmseg test cases for better coverage

--- vis4d/engine/trainer_test.py
+++ vis4d/engine/trainer_test.py
@@ -189,7 +189,7 @@ class TestSemSegMM(BaseEngineTests.TestTrain):
         """Set up class."""
         cls.work_dir = "./unittests/unittest_sem_seg_mm/"
         args = Namespace(
-            config=get_test_file("segment/deeplabv3_mmseg.toml"),
+            config=get_test_file("segment/fpn_mmseg.toml"),
             work_dir=cls.work_dir,
         )
         cls.cfg = config.parse_config(args)
