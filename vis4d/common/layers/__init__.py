e1f9ab1ff7eb9e529d150d4edabade2ac4ab12a5
Thomas Huang <thomas.huang@vision.ee.ethz.ch>
2022-01-09 18:59:21 +0100

    Moving deform conv to common layers

--- vis4d/common/layers/__init__.py
+++ vis4d/common/layers/__init__.py
@@ -1,5 +1,6 @@
 """Init layers module."""
 
 from .conv2d import Conv2d, add_conv_branch
+from .deconv import DeformConv
 
-__all__ = ["Conv2d", "add_conv_branch"]
+__all__ = ["Conv2d", "add_conv_branch", "DeformConv"]
