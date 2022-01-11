e8479c8a82912ced6ab6ff8c0fd60e5959bf0489
Thomas Huang <thomas.huang@vision.ee.ethz.ch>
2022-01-11 13:11:41 +0100

    Renaming deform conv name

--- vis4d/common/layers/__init__.py
+++ vis4d/common/layers/__init__.py
@@ -1,6 +1,6 @@
 """Init layers module."""
 
 from .conv2d import Conv2d, add_conv_branch
-from .deconv import DeformConv
+from .deform_conv import DeformConv
 
 __all__ = ["Conv2d", "add_conv_branch", "DeformConv"]
