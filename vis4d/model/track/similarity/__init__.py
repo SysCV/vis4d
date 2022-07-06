e7456375f781a78f5450074d12f13f9696b5c659
René Zurbrügg <zrene@ethz.ch>
2022-07-06 09:43:53 +0200

    Added similarity graph to init

--- vis4d/model/track/similarity/__init__.py
+++ vis4d/model/track/similarity/__init__.py
@@ -1,5 +1,6 @@
 """RoI heads."""
 from .base import BaseSimilarityHead
+from .deepsort import DeepSortSimilarityHead
 from .qdtrack import QDSimilarityHead
 
-__all__ = ["BaseSimilarityHead", "QDSimilarityHead"]
+__all__ = ["BaseSimilarityHead", "QDSimilarityHead", "DeepSortSimilarityHead"]
