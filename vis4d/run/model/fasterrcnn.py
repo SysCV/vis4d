ad6e6bef78930c7c597b34092bd4f339b0c1f45c
Fisher Yu <i@yf.io>
2022-10-03 14:47:47 +0200

    add run

---
+++ vis4d/run/model/fasterrcnn.py
@@ -0,0 +1,1 @@
+python -m vis4d.run.model.fasterrcnn
---
+++ vis4d/run/train.py
@@ -0,0 +1,74 @@
+"""Vis4D trainer."""
+from time import perf_counter
+from typing import List, Optional
+
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+
+from vis4d.eval import Evaluator
+from vis4d.optim.warmup import BaseLRWarmup
+
+from .test import testing_loop
+
+
+def training_loop(
+    train_dataloader: DataLoader,
+    test_dataloader: List[DataLoader],
+    evaluators: List[Evaluator],
+    metric: str,
+    model: nn.Module,
+    optimizer: optim.Optimizer,
+    scheduler: optim.lr_scheduler._LRScheduler,
+    num_epochs: int,
+    log_step: int,
+    learning_rate: float,
+    save_prefix: str,
+    warmup: Optional[BaseLRWarmup] = None,
+) -> None:
+    """Training loop."""
+    running_losses = {}
+    for epoch in range(num_epochs):
+        model.train()
+        for i, data in enumerate(train_dataloader):
+            tic = perf_counter()
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            losses = model(data)
+            total_loss = sum(losses.values())
+            total_loss.backward()
+
+            if warmup is not None:
+                if epoch == 0 and i < 500:
+                    for g in optimizer.param_groups:
+                        g["lr"] = warmup(i, learning_rate)
+                elif epoch == 0 and i == 500:
+                    for g in optimizer.param_groups:
+                        g["lr"] = learning_rate
+
+            optimizer.step()
+            toc = perf_counter()
+
+            # print statistics
+            losses = dict(time=toc - tic, loss=total_loss, **losses)
+            for k, v in losses.items():
+                if k in running_losses:
+                    running_losses[k] += v
+                else:
+                    running_losses[k] = v
+            if i % log_step == (log_step - 1):
+                log_str = (
+                    f"[{epoch + 1}, {i + 1:5d} / {len(train_dataloader)}] "
+                )
+                for k, v in running_losses.items():
+                    log_str += f"{k}: {v / log_step:.3f}, "
+                print(log_str.rstrip(", "))
+                running_losses = {}
+
+        scheduler.step()
+        torch.save(model.state_dict(), f"{save_prefix}_{epoch + 1}.pt")
+        testing_loop(test_dataloader, evaluators, metric, model)
+    print("training done.")
