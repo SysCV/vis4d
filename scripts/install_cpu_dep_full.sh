25d823c33424f62dff0aa256497b7077923dafa2
zrene <rene.zurbruegg@gmail.com>
2022-11-01 13:31:07 +0100

    Adding optional dependency back

--- scripts/install_cpu_dep_full.sh
+++ scripts/install_cpu_dep_full.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python3 -m pip install -r requirements/base.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
-# python3 -m pip install -r requirements/optional.txt
+python3 -m pip install -r requirements/optional.txt
 python3 -m pip install -r requirements/dev.txt
 python3 -m pip install -r requirements/visualization.txt
