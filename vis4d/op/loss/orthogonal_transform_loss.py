e183dc2ccd7e3e4722c8c2387f1a827934c06a7b
zrene <rene.zurbruegg@gmail.com>
2022-11-21 16:32:45 +0100

    lint,typing and doc

--- vis4d/op/loss/orthogonal_transform_loss.py
+++ vis4d/op/loss/orthogonal_transform_loss.py
@@ -1,31 +1,60 @@
-"""Orthogonal Transform_loss."""
-from typing import List
+"""Orthogonal Transform Loss."""
+from __future__ import annotations
 
 import torch
-import torch.nn as nn
 
 from .base import Loss
 
 
 class OrthogonalTransformRegularizationLoss(Loss):
-    """Loss that punishes linear transformations that are not orthogonal."""
+    """Loss that punishes linear transformations that are not orthogonal.
 
-    def forward(self, transforms: List[torch.Tensor]) -> torch.Tensor:
-        """Calculates the loss."""
-        loss = 0
+    Calculates difference of X'*X and identity matrix using norm( X'*X - I)
+    """
+
+    def __call___(self, transforms: list[torch.Tensor]) -> torch.Tensor:
+        """Calculates the loss.
+
+        Calculates difference of X'*X and the identity matrix using
+        norm(X'*X - I) for each transformation
+
+        Args:
+            transforms: (list(torch.tensor)) list with transformation matrices
+                        batched ([N, 3, 3], [N, x, x], ....)
+
+        Returns:
+            torch.Tensor containing the mean loss value (mean(norm(X'*X - I)))
+        """
+        return self._call_impl(transforms)
+
+    def forward(self, transforms: list[torch.Tensor]) -> torch.Tensor:
+        """Calculates the loss.
+
+        Calculates difference of X'*X and the identity matrix using
+        norm(X'*X - I) for each transformation
+
+        Args:
+            transforms: (list(torch.tensor)) list with transformation matrices
+                        batched ([N, 3, 3], [N, x, x], ....)
+
+        Returns:
+            torch.Tensor containing the mean loss value (mean(norm(X'*X - I)))
+        """
+        loss = torch.tensor(0.0)
         for trans in transforms:
             d = trans.size()[1]
 
             try:
-                I = self.get_buffer(f"identity_{d}")
-            except AttributeError as e:
+                identity = self.get_buffer(f"identity_{d}")
+            except AttributeError as _:
                 # Create identity buffers if not yet allocated
-                I = torch.eye(d, device=trans.device)
-                self.register_buffer(f"identity_{d}", I)
+                identity = torch.eye(d, device=trans.device)
+                self.register_buffer(f"identity_{d}", identity)
 
             loss += torch.mean(
                 torch.norm(
-                    torch.bmm(trans, trans.transpose(2, 1)) - I, dim=(1, 2)
+                    torch.bmm(trans, trans.transpose(2, 1)) - identity,
+                    dim=(1, 2),
                 )
             )
         return loss
