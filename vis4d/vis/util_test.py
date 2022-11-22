9dfeb357eb70a11590001031ea33998378e11584
zrene <rene.zurbruegg@gmail.com>
2022-11-22 18:06:15 +0100

    Fixing unittests

--- vis4d/vis/util_test.py
+++ vis4d/vis/util_test.py
@@ -6,15 +6,12 @@ import numpy as np
 import torch
 from PIL import Image
 
-from vis4d.struct_to_revise import Intrinsics
 from vis4d.unittest.utils import generate_dets, generate_instance_masks
 
 from .util import (
-    box3d_to_corners,
     generate_colors,
     preprocess_boxes,
     preprocess_image,
-    preprocess_intrinsics,
     preprocess_masks,
 )
 
@@ -41,12 +38,6 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(proc_img.shape, (128, 128, 3))
         self.assertTrue(np.min(proc_img) >= 0)
         self.assertTrue(np.max(proc_img) < 256)
-
-    def test_preprocess_intrinsics(self) -> None:
-        """Test preprocess_intrinsics method."""
-        mat1 = preprocess_intrinsics(np.eye(3))
-        mat2 = preprocess_intrinsics(Intrinsics(torch.eye(3)))
-        self.assertTrue(np.isclose(mat1, mat2).all())
 
     def test_preprocess_boxes(self) -> None:
         """Test preprocess_boxes method."""
@@ -103,18 +94,3 @@ class TestUtils(unittest.TestCase):
         masks[0].class_ids = None
         proc_masks, cols = preprocess_masks(masks)
         self.assertTrue(len(set(cols)) == 1)
-
-    def test_box3d_to_corners(self) -> None:
-        """Test for box3d_to_corners function."""
-        box_corners = [
-            [11.0, 11.0, 8.0],
-            [9.0, 11.0, 8.0],
-            [9.0, 11.0, 12.0],
-            [11.0, 11.0, 12.0],
-            [11.0, 9.0, 8.0],
-            [9.0, 9.0, 8.0],
-            [9.0, 9.0, 12.0],
-            [11.0, 9.0, 12.0],
-        ]
-        corners = box3d_to_corners([10, 10, 10, 2, 2, 4, 0, math.pi / 2, 0])
-        self.assertTrue((corners == box_corners).all())
