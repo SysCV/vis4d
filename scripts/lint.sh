f7d03973046390bb2a3abbdbcb443a1946330e8e
tobiasfshr <tobias.fischer@vision.ee.ethz.ch>
2022-12-14 11:04:10 +0100

    add file

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,11 +1,10 @@
 python3 -m black vis4d
-python3 -m isort vis4d
-python3 -m pylint vis4d
-python3 -m pydocstyle --convention=google vis4d
-python3 -m mypy vis4d
-
 python3 -m black tests
+python3 -m isort vis4d
 python3 -m isort tests
+python3 -m pylint vis4d
 python3 -m pylint tests
+python3 -m pydocstyle --convention=google vis4d
 python3 -m pydocstyle --convention=google tests
+python3 -m mypy vis4d
 python3 -m mypy tests
