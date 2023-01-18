c02a002cd596a9cbaff9c2c925183c86d8d64fff
zrene <rene.zurbruegg@gmail.com>
2023-01-18 16:47:02 +0100

    Add docstring to tests

--- tests/common/array_test.py
+++ tests/common/array_test.py
@@ -12,21 +12,26 @@ class TestConvertToArray(unittest.TestCase):
     """Test cases for array conversion ops."""
 
     def test_none(self) -> None:
+        """Test that None is returned as None."""
         self.assertEqual(array_to_numpy(None), None)
 
     def test_array(self) -> None:
+        """Test that numpy arrays are returned as is."""
         data = np.random.rand(2, 3)
         self.assertFalse((data - array_to_numpy(data)).any())
 
     def test_iterable(self) -> None:
+        """Test that iterables are converted to numpy arrays."""
         data = [[1, 2, 3], [2, 3, 4]]
         self.assertFalse((np.asarray(data) - array_to_numpy(data)).any())
 
     def test_torch(self) -> None:
+        """Test that torch tensors are converted to numpy arrays."""
         data = np.asarray([[1, 2, 3], [2, 3, 4]])
         self.assertFalse((data - array_to_numpy(torch.from_numpy(data))).any())
 
     def test_dim_shaping(self) -> None:
+        """Test that the number of dimensions is correct."""
         data = np.random.rand(2, 3)
 
         # Check expanding works
@@ -42,6 +47,7 @@ class TestConvertToArray(unittest.TestCase):
         self.assertEqual(array_to_numpy(data.copy(), 2).shape, (2, 3))
 
     def test_array_to_numpys_multiple(self) -> None:
+        """Test that multiple arrays are converted correctly."""
         out = arrays_to_numpy(
             np.random.rand(2, 3), np.random.rand(1, 1, 2, 3), n_dims=3
         )
