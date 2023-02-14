0983c12c957004b586adc13d09d4126e110ac262
suniique <taosun47@student.ethz.ch>
2023-02-15 00:58:45 +0100

    add one view shift dataset

--- vis4d/data/datasets/shift.py
+++ vis4d/data/datasets/shift.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import os
 
+from collections.abc import Sequence
 from vis4d.common.imports import SCALABEL_AVAILABLE
 from vis4d.data.const import CommonKeys
 
@@ -47,8 +48,45 @@ if SCALABEL_AVAILABLE:
     from scalabel.label.typing import Dataset as ScalabelData
 
 
+class SHIFT_ONE_VIEWS(Scalabel):
+    """SHIFT dataset, based on Scalabel."""
+
+    def __init__(
+        self,
+        data_root: str,
+        split: str,
+        keys_to_load: Sequence[str] = (CommonKeys.images, CommonKeys.boxes2d),
+        view_to_load: str = "front",
+    ) -> None:
+        """Initialize SHIFT ONE VIEWS dataset."""
+        self.annotation_path = os.path.join(
+            self.data_root, "discrete", "", f"{self.split}.json"
+        )
+
+    def _generate_mapping(self) -> ScalabelData:
+        """Generate data mapping."""
+        shift_anns = load(self.annotation_path, validate_frames=False)
+        return ScalabelData(frames=shift_anns.frames, config=shift_anns.config)
+
+
 class SHIFT(Dataset):
     """SHIFT dataset, based on Scalabel."""
+
+    KEYS = [
+        CommonKeys.images,
+        CommonKeys.original_hw,
+        CommonKeys.input_hw,
+        CommonKeys.intrinsics,
+        CommonKeys.extrinsics,
+        CommonKeys.timestamp,
+        CommonKeys.axis_mode,
+        CommonKeys.boxes2d,
+        CommonKeys.boxes2d_classes,
+        CommonKeys.boxes2d_track_ids,
+        CommonKeys.boxes3d,
+        CommonKeys.boxes3d_classes,
+        CommonKeys.boxes3d_track_ids,
+    ]
 
     def __init__(
         self,
