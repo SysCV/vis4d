4efb7c5ef1059cf65f3d202dc882aef94217bac1
zrene <rene.zurbruegg@gmail.com>
2023-02-19 22:38:05 +0100

    fix import

--- vis4d/config/example/show_connections.py
+++ vis4d/config/example/show_connections.py
@@ -2,7 +2,7 @@
 from vis4d.config.data_graph import prints_datagraph_for_config
 
 # from vis4d.config.example.faster_rcnn_coco import get_config
-from vis4d.config.example.fcn_segment import get_config
+from vis4d.config.example.fcn_segment_coco import get_config
 from vis4d.config.util import instantiate_classes
 
 dg = prints_datagraph_for_config(instantiate_classes(get_config()))
