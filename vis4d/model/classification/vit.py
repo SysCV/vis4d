ab07850af33d24eccee5d9dc7111b90bd662ae06
suniique <taosun47@student.ethz.ch>
2023-02-22 16:44:59 +0100

    add vit model

---
+++ vis4d/model/classification/vit.py
@@ -0,0 +1,27 @@
+import vis4d.op.base.vit import ViT
+from torch import nn
+import torch
+
+
+class ClassificationViT(nn.Module):
+    """ViT for classification tasks."""
+
+    def __init__(self, num_classes: int, *args, **kwargs) -> None:
+        """Initialize the classification ViT.
+
+        Args:
+            num_classes (int): Number of classes.
+            *args: Arguments passed to the ViT.
+            **kwargs: Keyword arguments passed to the ViT.
+        """
+        super().__init__(*args, **kwargs)
+        self.num_classes = num_classes
+        self.vit = ViT(*args, **kwargs)
+        self.classifier = nn.Linear(self.vit.out_channels[-1], num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward pass."""
+        _, feats = self.vit(x)
+        y = self.classifier(feats)
+        return y
+
