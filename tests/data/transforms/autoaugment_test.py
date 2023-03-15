fe4488096415b327f5610e888da56a71b36466d4
suniique <taosun47@student.ethz.ch>
2023-03-15 14:58:42 +0100

    update autoaug to functional style

--- tests/data/transforms/autoaugment_test.py
+++ tests/data/transforms/autoaugment_test.py
@@ -2,14 +2,14 @@
 import torch
 
 from vis4d.common.util import set_random_seed
-from vis4d.data.transforms.autoaugment import autoaugment
+from vis4d.data.transforms.autoaugment import augmix, autoaug, randaug
 
 
 def test_autoaugment() -> None:
     """Auto augment testcase."""
     set_random_seed(0, deterministic=True)
 
-    transform = autoaugment("original", params={})
+    transform = autoaug(policy="original", magnitude_std=0.5)
     batch_size = 4
     x = 120 * torch.ones((batch_size, 3, 32, 32), dtype=torch.uint8)
     data_dict = {"images": x}
@@ -18,14 +18,30 @@ def test_autoaugment() -> None:
 
     assert x["images"].shape == torch.Size([batch_size, 3, 32, 32])
     assert x["images"].min() == 120
-    assert x["images"].max() == 135
+    assert x["images"].max() == 128
 
 
 def test_randaugment() -> None:
     """Random augment testcase."""
     set_random_seed(0, deterministic=True)
 
-    transform = autoaugment("rand-m9-mstd12", params={})
+    transform = randaug(magnitude=9, magnitude_std=0.5)
+    batch_size = 4
+    x = 120 * torch.ones((batch_size, 3, 32, 32), dtype=torch.uint8)
+    data_dict = {"images": x}
+
+    x = transform(data_dict)
+
+    assert x["images"].shape == torch.Size([batch_size, 3, 32, 32])
+    assert x["images"].min() == 120
+    assert x["images"].max() == 128
+
+
+def test_randaugment_increasing() -> None:
+    """Random augment with increasing transforms testcase."""
+    set_random_seed(0, deterministic=True)
+
+    transform = randaug(magnitude=9, use_increasing=True, magnitude_std=0.5)
     batch_size = 4
     x = 120 * torch.ones((batch_size, 3, 32, 32), dtype=torch.uint8)
     data_dict = {"images": x}
@@ -41,7 +57,7 @@ def test_mixaugment() -> None:
     """Random mix augment testcase."""
     set_random_seed(0, deterministic=True)
 
-    transform = autoaugment("augmix-m9-mstd12", params={})
+    transform = augmix(magnitude=9, magnitude_std=12, alpha=0.5)
     batch_size = 4
     x = 120 * torch.ones((batch_size, 3, 32, 32), dtype=torch.uint8)
     data_dict = {"images": x}
@@ -49,5 +65,5 @@ def test_mixaugment() -> None:
     x = transform(data_dict)
 
     assert x["images"].shape == torch.Size([batch_size, 3, 32, 32])
-    assert x["images"].min() == 96
-    assert x["images"].max() == 122
+    assert x["images"].min() == 48
+    assert x["images"].max() == 125
--- vis4d/data/transforms/autoaugment.py
+++ vis4d/data/transforms/autoaugment.py
@@ -1,4 +1,6 @@
 """A wrap for timm transforms."""
+from typing import Union
+
 import numpy as np
 import torch
 from PIL import Image
@@ -10,9 +12,29 @@ from .base import Transform
 
 if TIMM_AVAILABLE:
     from timm.data.auto_augment import (
-        augment_and_mix_transform,
-        auto_augment_transform,
-        rand_augment_transform,
+        _RAND_INCREASING_TRANSFORMS,
+        _RAND_TRANSFORMS,
+        AugMixAugment,
+        AutoAugment,
+        RandAugment,
+        augmix_ops,
+        auto_augment_policy,
+        rand_augment_ops,
+    )
+
+AugOp = Union[AutoAugment, RandAugment, AugMixAugment]
+
+
+def _apply_aug(images: torch.Tensor, aug_op: AugOp) -> torch.Tensor:
+    """Apply augmentation to a batch of images."""
+    pil_imgs = [
+        Image.fromarray(image)
+        for image in images.permute(0, 2, 3, 1).cpu().numpy()
+    ]
+    for i, img in enumerate(pil_imgs):
+        pil_imgs[i] = aug_op(img)
+    return torch.stack(
+        [torch.from_numpy(np.array(img)).permute(2, 0, 1) for img in pil_imgs]
     )
 
 
@@ -20,16 +42,14 @@ if TIMM_AVAILABLE:
     in_keys=(K.images,),
     out_keys=(K.images,),
 )
-def autoaugment(policy: str, params: dict[str, float]):
+def autoaug(policy: str, magnitude_std: float = 0.5):
     """Apply timm's autoaugment to a image tensor.
 
     Args:
-        policy (str): Policy name for autoaugment. String defining
-            configuration of auto augmentation. Consists of multiple sections
-            separated by dashes ('-'). The first section defines the
-            AutoAugment policy o use. The remaining sections, if present,
-            define the augmentation parameters.
-        params (dict[str, float], optional): Parameters for the policy.
+        policy (str): Policy name for autoaugment. Options are "original",
+            "originalr", "v0", "v0r".
+        magnitude_std (float, optional): Standard deviation of the magnitude
+            for random autoaugment. Defaults to 0.5.
 
     Returns:
         Callable: A function that takes a tensor of shape [N, C, H, W] and
@@ -37,26 +57,20 @@ def autoaugment(policy: str, params: dict[str, float]):
 
     Example:
         Original autoaugment policy (`https://arxiv.org/abs/1805.09501`)
-        >>> autoaugment("original")
-
-        Autoaugment policy used in EfficientNet implementation
-        >>> autoaugment("v0")
-
-        Random autoaugment policy (`https://arxiv.org/abs/1909.13719`)
-        >>> autoaugment("rand-m9-mstd0.5")
-
-        AugMix policy
-        >>> autoaugment("augmix-m1-mstd0.5")
+        >>> autoaugment("original", magnitude_std=0.5)
     """
+    assert TIMM_AVAILABLE, "timm is not installed."
+    assert policy in {
+        "original",
+        "originalr",
+        "v0",
+        "v0r",
+    }, "Policy must be one of 'original', 'originalr', 'v0', 'v0r'."
 
     def _autoaugment(images: torch.Tensor) -> torch.Tensor:
-        if policy.startswith("rand"):
-            timm_transform = rand_augment_transform
-        elif policy.startswith("augmix"):
-            timm_transform = augment_and_mix_transform
-        else:
-            timm_transform = auto_augment_transform
-        aug_op = timm_transform(policy, params)
+        hparams = {"magnitude_std": magnitude_std}
+        aa_policy = auto_augment_policy(policy, hparams=hparams)
+        aug_op = AutoAugment(aa_policy)
         pil_imgs = [
             Image.fromarray(image)
             for image in images.permute(0, 2, 3, 1).cpu().numpy()
@@ -71,3 +85,94 @@ def autoaugment(policy: str, params: dict[str, float]):
         )
 
     return _autoaugment
+
+
+@Transform(
+    in_keys=(K.images,),
+    out_keys=(K.images,),
+)
+def randaug(
+    magnitude: int,
+    num_layers: int = 2,
+    use_increasing: bool = False,
+    magnitude_std: float = 0.5,
+):
+    """Apply timm's rand augment to a image tensor.
+
+    Args:
+        magnitude (int): Level of magnitude for augments, ranging from 1 to 9.
+        num_layers (int, optional): Number of layers for rand augment. Defaults
+            to 2.
+        use_increasing (bool, optional): Whether to use increasing setting for
+            transforms. Defaults to False.
+        magnitude_std (float, optional): Standard deviation of the magnitude
+            for random autoaugment. Defaults to 0.5.
+
+    Returns:
+        Callable: A function that takes a tensor of shape [N, C, H, W] and
+            returns a tensor of the same shape.
+
+    Example:
+        Rand augment with magnitude 9. (`https://arxiv.org/abs/1909.13719`)
+        >>> rand_augment(magnitude=9)
+    """
+    assert TIMM_AVAILABLE, "timm is not installed."
+
+    def _rand_augment(images: torch.Tensor) -> torch.Tensor:
+        hparams = {"magnitude_std": magnitude_std}
+        if use_increasing:
+            transforms = _RAND_INCREASING_TRANSFORMS
+        else:
+            transforms = _RAND_TRANSFORMS
+        ra_ops = rand_augment_ops(
+            magnitude=magnitude, hparams=hparams, transforms=transforms
+        )
+        aug_op = RandAugment(ra_ops, num_layers)
+        return _apply_aug(images, aug_op)
+
+    return _rand_augment
+
+
+@Transform(
+    in_keys=(K.images,),
+    out_keys=(K.images,),
+)
+def augmix(
+    magnitude: int,
+    width: int = 3,
+    alpha: float = 1.0,
+    depth: int = -1,
+    blended: bool = True,
+    magnitude_std: float = 0.5,
+):
+    """Apply timm's augmix to a image tensor.
+
+    Args:
+        magnitude (int): Level of magnitude for augments, ranging from 1 to 9.
+        width (int, optional): Width of the augmentation chain. Defaults to 3.
+        alpha (float, optional): Alpha for beta distribution. Defaults to 1.0.
+        depth (int, optional): Depth of the augmentation chain. Defaults to -1.
+        blended (bool, optional): Whether to blend the original image with the
+            augmented image. Defaults to True.
+        magnitude_std (float, optional): Standard deviation of the magnitude
+            for random autoaugment. Defaults to 0.5.
+
+    Returns:
+        Callable: A function that takes a tensor of shape [N, C, H, W] and
+            returns a tensor of the same shape.
+
+    Example:
+        Augmix with magnitude 9. (`https://arxiv.org/abs/1912.02781`)
+        >>> augmix(magnitude=9)
+    """
+    assert TIMM_AVAILABLE, "timm is not installed."
+
+    def _augmix(images: torch.Tensor) -> torch.Tensor:
+        hparams = {"magnitude_std": magnitude_std}
+        am_ops = augmix_ops(magnitude=magnitude, hparams=hparams)
+        aug_op = AugMixAugment(
+            am_ops, alpha=alpha, width=width, depth=depth, blended=blended
+        )
+        return _apply_aug(images, aug_op)
+
+    return _augmix
