7056cc16f46a0b7ee337411bc677a1ddb07d5407
suniique <taosun47@student.ethz.ch>
2023-04-04 21:14:29 +0800

    add random erasing bypass test

--- tests/data/transforms/random_erasing_test.py
+++ tests/data/transforms/random_erasing_test.py
@@ -26,3 +26,16 @@ def test_random_erasing() -> None:
         assert np.isclose(x[i, 1:].sum(), np.array(0.0))
 
     assert x.shape == (batch_size, 3, 10, 10)
+
+
+def test_random_erasing_bypass() -> None:
+    """Random erasing testcase."""
+    transform = RandomErasing(
+        probability=0.0,
+    )
+    batch_size = 4
+    x_ori = np.random.randn(batch_size, 3, 10, 10).astype(np.float32)
+
+    x = transform(x_ori)
+
+    assert (x == x_ori).all()
