36bb3221580c09ec1f172b3680566d9a3d940087
Fisher Yu <i@yf.io>
2023-06-27 16:52:24 +0200

    put detectron2 into a separate file

--- scripts/install_cpu_dep_full.sh
+++ scripts/install_cpu_dep_full.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 -m pip install --ignore-installed -r requirements/install.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
-python3 -m pip install --ignore-installed -r requirements/dev.txt
+python3 -m pip install -r requirements/install.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
+python3 -m pip install -r requirements/torch-lib.txt
+python3 -m pip install -r requirements/dev.txt
