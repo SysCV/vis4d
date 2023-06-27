bb65c2192fcfa4202cc36d5aec085957810d5c78
Fisher Yu <i@yf.io>
2023-06-27 16:34:51 +0200

    Clean requirements

--- scripts/install_cpu_dep_full.sh
+++ scripts/install_cpu_dep_full.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-python3 -m pip install --ignore-installed -r requirements/base.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
-python3 -m pip install --ignore-installed -r requirements/optional.txt
+python3 -m pip install --ignore-installed -r requirements/install.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html
 python3 -m pip install --ignore-installed -r requirements/dev.txt
-python3 -m pip install --ignore-installed -r requirements/visualization.txt
-python3 -m pip install --ignore-installed -r requirements/datasets.txt
-python3 -m pip install -r requirements/external.txt
