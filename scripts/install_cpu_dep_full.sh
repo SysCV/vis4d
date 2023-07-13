36734e063ed6d93b0e2a8f6f77f5d016fe0b401c
zrene <rene.zurbruegg@gmail.com>
2023-07-13 09:11:20 +0200

    fix

--- scripts/install_cpu_dep_full.sh
+++ scripts/install_cpu_dep_full.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m pip install -r requirements/install.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html -f pip install torch-scatter -f https://data.pyg.org/whl/torch-2.0.0+cpu.html
+python3 -m pip install -r requirements/install.txt -f https://download.pytorch.org/whl/cpu/torch_stable.html -f https://data.pyg.org/whl/torch-2.0.0+cpu.html
 python3 -m pip install -r requirements/torch-lib.txt
 python3 -m pip install -r requirements/viewer.txt
 python3 -m pip install -r requirements/dev.txt
