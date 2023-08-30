071ed695ec1d04744bd87dc8a74adf8e94f70141
zrene <rene.zurbruegg@gmail.com>
2023-08-30 22:53:33 +0200

    lint

--- vis4d/zoo/__init__.py
+++ vis4d/zoo/__init__.py
@@ -1,6 +1,8 @@
 """Model Zoo."""
 from __future__ import annotations
 
+from vis4d.common.typing import ArgsType
+
 from .bdd100k import AVAILABLE_MODELS as BDD100K_MODELS
 from .bevformer import AVAILABLE_MODELS as BEVFORMER_MODELS
 from .cc_3dt import AVAILABLE_MODELS as CC_3DT_MODELS
@@ -13,7 +15,7 @@ from .shift import AVAILABLE_MODELS as SHIFT_MODELS
 from .vit import AVAILABLE_MODELS as VIT_MODELS
 from .yolox import AVAILABLE_MODELS as YOLOX_MODELS
 
-AVAILABLE_MODELS = {
+AVAILABLE_MODELS: dict[str, dict[str, ArgsType]] = {
     "bdd100k": BDD100K_MODELS,
     "cc_3dt": CC_3DT_MODELS,
     "bevformer": BEVFORMER_MODELS,
