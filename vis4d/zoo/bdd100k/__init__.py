3e29c3f3f20d53eff84a3255cf91e6c05dd7ff6f
RoyYang0714 <sy2es93104@gmail.com>
2023-09-01 16:43:44 +0200

    fix: Fix isort.

--- vis4d/zoo/bdd100k/__init__.py
+++ vis4d/zoo/bdd100k/__init__.py
@@ -5,15 +5,15 @@ from .mask_rcnn import (
     mask_rcnn_r50_3x_bdd100k,
     mask_rcnn_r50_5x_bdd100k,
 )
-from .semantic_fpn import (
-    semantic_fpn_r50_40k_bdd100k,
-    semantic_fpn_r50_80k_bdd100k,
-    semantic_fpn_r101_80k_bdd100k,
-)
 from .qdtrack import (
     qdtrack_frcnn_r50_fpn_1x_bdd100k,
     qdtrack_yolox_s_50e_bdd100k,
     qdtrack_yolox_x_50e_bdd100k,
+)
+from .semantic_fpn import (
+    semantic_fpn_r50_40k_bdd100k,
+    semantic_fpn_r50_80k_bdd100k,
+    semantic_fpn_r101_80k_bdd100k,
 )
 
 # Lists of available models in BDD100K Model Zoo.
